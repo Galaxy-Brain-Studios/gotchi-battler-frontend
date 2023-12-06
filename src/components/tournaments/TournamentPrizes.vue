bd8d046739f1bfc34d38ab9046b4681befd707f9
Mistaya <10831655+mistaya@users.noreply.github.com>
2023-12-06 13:57:04 +0000

    Add prize mockdata, edge cases, DAI icon, optional custom label

--- src/components/tournaments/TournamentPrizes.vue
+++ src/components/tournaments/TournamentPrizes.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+  import SiteIcon from "../common/SiteIcon.vue"
   defineProps({
     tournament: {
       type: Object,
@@ -7,29 +7,48 @@
     }
   })
 
-  const placements = [
+  // Prize data includes 'place' which is a 1-based integer
+  // Note that these labels are 0-based
+  const defaultPlacementLabels = [
     '1st', '2nd', '3rd - 4th', '5th - 8th', '9th - 16th', '17th - 32nd', '33rd - 64th', '65th - 128th', '129th - 256th', '257th - 512th', '513th - 1024th', '1025th - 2048th'
   ]
 
-  const currencyImgs = {
-    dai: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.svg?v=029'
+  const currencyIcons = {
+    dai: 'token-dai'
   }
 </script>
 
 <template>
   <div>
-    <table class="prizes-table">
+    <div
+      v-if="!tournament?.prizes?.length"
+      class="prizes-table__empty"
+    >
+      No prizes are available for this tournament.
+    </div>
+    <table
+      v-else
+      class="prizes-table"
+    >
       <tbody>
         <tr v-for="(prize, i) in tournament.prizes" :key="prize.place">
           <td class="prizes-table__icon">
             {{ i === 0 ? '🥇' : i === 1 ? '🥈' : i === 2 ? '🥉' : '' }}
           </td>
           <td class="prizes-table__place">
-            {{ placements[prize.place + 1] }}
+            {{ prize.label || defaultPlacementLabels[prize.place - 1] }}
           </td>
           <td class="prizes-table__prize">
-            <img v-if="currencyImgs[prize.currency]" :src="currencyImgs[prize.currency]" :alt="prize.currency" width="24">
+            <SiteIcon
+              v-if="currencyIcons[prize.currency]"
+              :name="currencyIcons[prize.currency]"
+              :alt="prize.currency"
+              class="prizes-table__currency-icon"
+            />
             {{ prize.prize }}
+            <template v-if="!currencyIcons[prize.currency]">
+              {{ prize.currency }}
+            </template>
           </td>
         </tr>
       </tbody>
@@ -38,15 +57,16 @@
 </template>
 
 <style scoped>
+.prizes-table__empty {
+  color: var(--c-white);
+  font-size: 1.5rem;
+}
+
 .prizes-table {
   color: var(--c-white);
   font-size: 1.5rem;
   margin: 0 auto;
   margin-top: 5rem;
-}
-
-.prizes-table th span {
-  opacity: 0.8;
 }
 
 .prizes-table td {
@@ -65,7 +85,7 @@
   font-size: 1.75rem;
 }
 
-.prizes-table__prize img{
+.prizes-table__currency-icon{
   margin-right: 0.5rem;
 }
 </style>

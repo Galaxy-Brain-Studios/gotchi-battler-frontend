ae481e1165f5328eb173112ae6befab39aec7c01
Mistaya <10831655+mistaya@users.noreply.github.com>
2024-10-10 15:06:34 +0100

    Update api urls to use v1

--- src/data/api.js
+++ src/data/api.js
@@ -23,11 +23,11 @@ const urls = {
   trainingTeams: () =>  `${BASE_URL}/api/v1/training/teams/`,
   team: (id) => `${BASE_URL}/api/v1/teams/${encodeURIComponent(id)}`,
   createTournamentTeam: (tournamentId) =>
-    `${BASE_URL}/api/v2/me/tournaments/${encodeURIComponent(tournamentId)}/teams`,
+    `${BASE_URL}/api/v1/me/tournaments/${encodeURIComponent(tournamentId)}/teams`,
   deleteTournamentTeam: ({ tournamentId, teamId }) =>
-    `${BASE_URL}/api/v2/me/tournaments/${encodeURIComponent(tournamentId)}/teams/${encodeURIComponent(teamId)}`,
+    `${BASE_URL}/api/v1/me/tournaments/${encodeURIComponent(tournamentId)}/teams/${encodeURIComponent(teamId)}`,
   editTournamentTeam: ({ tournamentId, teamId }) =>
-    `${BASE_URL}/api/v2/me/tournaments/${encodeURIComponent(tournamentId)}/teams/${encodeURIComponent(teamId)}`,
+    `${BASE_URL}/api/v1/me/tournaments/${encodeURIComponent(tournamentId)}/teams/${encodeURIComponent(teamId)}`,
   tournamentTeams: (tournamentId) => `${BASE_URL}/api/v1/tournaments/${encodeURIComponent(tournamentId)}/teams`,
   tournamentTeamsReport: (tournamentId) => `${BASE_URL}/api/v1/tournaments/${encodeURIComponent(tournamentId)}/teams-report`,
   tournamentGotchis: (tournamentId) => `${BASE_URL}/api/v1/tournaments/${encodeURIComponent(tournamentId)}/gotchis`,
@@ -38,19 +38,19 @@ const urls = {
   trainingGotchis: () =>  `${BASE_URL}/api/v1/training/gotchis`,
   gotchis: ({ address }) =>  `${BASE_URL}/api/v1/gotchis/${encodeURIComponent(address)}`,
   availableLendings: (tournamentId) => `${BASE_URL}/api/v1/tournaments/${encodeURIComponent(tournamentId)}/lendings`,
-  profile: (address) => `${BASE_URL}/api/v2/profile/${encodeURIComponent(address)}`,
-  profileTeams: () => `${BASE_URL}/api/v2/me/teams`,
-  profileInventory: () => `${BASE_URL}/api/v2/me/inventory`,
-  profileInventoryItemCount: (itemId) => `${BASE_URL}/api/v2/me/inventory/item/${encodeURIComponent(itemId)}/count`,
-  updateProfile: () => `${BASE_URL}/api/v2/me`,
-  deleteProfileTeam: ({ teamId }) => `${BASE_URL}/api/v2/me/team/${encodeURIComponent(teamId)}/delete`,
-  deleteProfileImage: () => `${BASE_URL}/api/v2/me/image/delete`,
+  profile: (address) => `${BASE_URL}/api/v1/profile/${encodeURIComponent(address)}`,
+  profileTeams: () => `${BASE_URL}/api/v1/me/teams`,
+  profileInventory: () => `${BASE_URL}/api/v1/me/inventory`,
+  profileInventoryItemCount: (itemId) => `${BASE_URL}/api/v1/me/inventory/item/${encodeURIComponent(itemId)}/count`,
+  updateProfile: () => `${BASE_URL}/api/v1/me`,
+  deleteProfileTeam: ({ teamId }) => `${BASE_URL}/api/v1/me/team/${encodeURIComponent(teamId)}/delete`,
+  deleteProfileImage: () => `${BASE_URL}/api/v1/me/image/delete`,
   generateImageUploadUrl: () => `${BASE_URL}/api/v1/me/avatar-upload-url`,
   finishProfileImageUpload: () => `${BASE_URL}/api/v1/me/avatar`,
-  sessionNonce: () => `${BASE_URL}/api/v2/auth/nonce`,
-  sessionLogin: () => `${BASE_URL}/api/v2/auth/login`,
-  sessionLogout: () => `${BASE_URL}/api/v2/auth/logout`,
-  shopItems: () => `${BASE_URL}/api/v2/shop/items`
+  sessionNonce: () => `${BASE_URL}/api/v1/auth/nonce`,
+  sessionLogin: () => `${BASE_URL}/api/v1/auth/login`,
+  sessionLogout: () => `${BASE_URL}/api/v1/auth/logout`,
+  shopItems: () => `${BASE_URL}/api/v1/shop/items`
 }
 
 export { api, apiText, apiWithCredentials, apiTextWithCredentials, urls, setBaseUrl }
